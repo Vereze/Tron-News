b7467035fc3a620c126088a9d76482de6f5bd496
Vereze <73610679+Vereze@users.noreply.github.com>
2021-06-04 20:01:14 -0300

    Add files via upload

---
+++ First folder ever/enlaces/Practice.js
@@ -0,0 +1,25 @@
+var okeybutton;
+var yesbutton;
+var nobutton;
+
+okeybutton = document.getElementById("okey-button");
+
+okeybutton.addEventListener("click", function(){
+    alert("OKEY")
+});
+
+yesbutton = document.getElementById("yes-buton");
+
+yesbutton.addEventListener("click", function(){
+    alert("Bueena cabro")
+});
+
+nobutton = document.getElementById("no-buton");
+
+nobutton.addEventListener("click", function (){
+    alert("Andate ya")
+});
+
+
+
+
---
+++ First folder ever/pl.js
@@ -0,0 +1,6 @@
+console.log("hello world");
+
+document.querySelector('.menu-buton').addEventListener('click', () =>{
+    console.log("Hola personita")
+    document.querySelector('.nav').classList.toggle('show');
+})
